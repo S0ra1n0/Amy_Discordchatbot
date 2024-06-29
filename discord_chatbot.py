21460769e54d3713cd3c0438b98827edccfba662
s0ra1n0 <148192457+S0ra1n0@users.noreply.github.com>
2024-06-30 03:48:53 +0700

    Create discord_chatbot.py

---
+++ discord_chatbot.py
@@ -0,0 +1,39 @@
+import discord
+import chatting
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = chatting.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+def run():
+    TOKEN = "MTI1NTA2OTQ3NDM2OTk2MjA2Ng.G2ZEf1.-z-_eKedVzfSVbZSz3HU2cTSmDMKi2abdgrUXM"
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
